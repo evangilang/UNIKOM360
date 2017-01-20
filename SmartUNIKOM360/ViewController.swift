6f9f656869633c6eb1c85e75ba4ff96f7e15b13e
evangilang <evangilang96@email.unikom.ac.id>
2017-01-20 10:42:05 +0700

    Initial Commit

---
+++ SmartUNIKOM360/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartUNIKOM360
+//
+//  Created by Divisi CodeLabs on 1/20/17.
+//  Copyright © 2017 Divisi CodeLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
